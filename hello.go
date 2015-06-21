2e70b418c60dc95011fc26ba9a4ac7c59d4c5b96
Gunjan Patel <patelgunjan5@gmail.com>
2015-06-21 03:23:35 -0700

    adding user specific greeting

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package hello
 import (
 	"fmt"
 	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
@@ -10,5 +13,20 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, App Engine!")
+
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %v! You look messed up today :p", u)
+
+	//fmt.Fprint(w, "Hello, GoAppEngine!")
 }
